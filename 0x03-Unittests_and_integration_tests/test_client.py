01348ec4d3c3f49b8e6c167cd41c8b6cb7343024
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-17 20:20:01 +0200

    Update test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import unittest
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Placeholder test class for client tasks"""
+
+    def test_placeholder(self):
+        self.assertTrue(True)
