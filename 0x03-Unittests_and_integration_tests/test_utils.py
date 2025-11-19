7fdae43a87fdb1bb3c0ed12c4437aaee4149dcde
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-19 15:49:01 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,14 @@
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), repr(path[-1]))
+
+
+
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
@@ -14,3 +25,4 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
