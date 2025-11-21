c368182662bfad1d276a01d238eef28d26488b83
Moana <missn.hadebe@gmail.com>
2025-11-21 13:47:14 +0200

    Add ConversationViewSet and MessageViewSet with API routes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """
-Main project URLs
+Routes for conversations and messages
 """
 
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from .views import ConversationViewSet, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register(r"conversations", ConversationViewSet, basename="conversation")
+router.register(r"messages", MessageViewSet, basename="message")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("messaging_app.chats.urls")),
+    path("", include(router.urls)),
 ]
 
--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 """
-Routes for conversations and messages
+Main project URLs
 """
 
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from .views import ConversationViewSet, MessageViewSet
-
-router = routers.DefaultRouter()
-router.register(r"conversations", ConversationViewSet, basename="conversation")
-router.register(r"messages", MessageViewSet, basename="message")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include("messaging_app.chats.urls")),
 ]
 
