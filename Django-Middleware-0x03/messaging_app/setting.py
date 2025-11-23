9f9649b24523bbdefefd6839fc896b414b988c90
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-23 10:31:16 +0200

    Add RequestLoggingMiddleware to middleware settings

--- Django-Middleware-0x03/messaging_app/setting.py
+++ Django-Middleware-0x03/messaging_app/setting.py
@@ -9,3 +9,15 @@ INSTALLED_APPS = [
     'rest_framework',
     'chats',  # we’ll add this app next
 ]
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",  # <-- add this line
+]
