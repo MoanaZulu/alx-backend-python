cf1c8de8acc12887376ac8a3091acde1aa5ea239
Moana <missn.hadebe@gmail.com>
2025-11-23 11:49:16 +0200

    Define RestrictAccessByTimeMiddleware correctly in chats/middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,25 +1,3 @@
-import logging
-from datetime import datetime
-
-class RequestLoggingMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-        # Configure logger to write to requests.log
-        logging.basicConfig(
-            filename="requests.log",
-            level=logging.INFO,
-            format="%(message)s"
-        )
-
-    def __call__(self, request):
-        user = request.user if request.user.is_authenticated else "Anonymous"
-        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        logging.info(log_message)
-
-        response = self.get_response(request)
-        return response
-
-
 from datetime import datetime
 from django.http import HttpResponseForbidden
 
@@ -33,3 +11,4 @@ class RestrictAccessByTimeMiddleware:
         if current_hour < 6 or current_hour >= 21:
             return HttpResponseForbidden("Access to chat is restricted during these hours.")
         return self.get_response(request)
+
