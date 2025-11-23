77b822ff5332472ff294364d2b8327e46ce75a1c
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-23 11:55:20 +0200

    Add middleware for request logging and time restriction

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -87,3 +87,17 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",
+    "chats.middleware.RestrictAccessByTimeMiddleware",  # <-- must be here
+]
+
