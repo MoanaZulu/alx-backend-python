867b64d64f3788f0fdd38c20574ec77c4c35a2dd
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-25 16:55:21 +0200

    Create tests.py

---
+++ Django-single_orm-0x04/messaging/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+
+class MessagingSignalTests(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username="alice", password="password")
+        self.receiver = User.objects.create_user(username="bob", password="password")
+
+    def test_notification_created_on_message(self):
+        """Ensure a notification is created when a message is sent."""
+        message = Message.objects.create(sender=self.sender, receiver=self.receiver, content="Hello Bob!")
+        notification = Notification.objects.filter(user=self.receiver, message=message).first()
+        self.assertIsNotNone(notification)
+        self.assertFalse(notification.is_read)
