9867c2623a7262d577d8f8653cbf9549e2a06953
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-25 17:28:23 +0200

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -14,3 +14,19 @@ class Notification(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="notifications")
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+Objective: Log when a user edits a message and save the old content before the edit.
+
+Instructions:
+
+Add an edited field to the Message model to track if a message has been edited.
+
+Use the pre_save signal to log the old content of a message into a separate MessageHistory model before it’s updated.
+
+Display the message edit history in the user interface, allowing users to view previous versions of their messages.
+
+Repo:
+
+GitHub repository: alx-backend-python
+Directory: Django-signals_orm-0x04
+File: messaging/Models
