b0f99b2377ce5ee66b42d76e42a5428b597d3b7d
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-11-25 17:29:01 +0200

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -10,3 +10,32 @@ def create_notification(sender, instance, created, **kwargs):
     """
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from .models import Message, Notification, MessageHistory
+
+
+@receiver(post_save, sender=Message)
+def create_notification(sender, instance, created, **kwargs):
+    """Create a notification when a new message is sent."""
+    if created:
+        Notification.objects.create(user=instance.receiver, message=instance)
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """Log old content before a message is updated."""
+    if instance.pk:  # Only if the message already exists
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                # Save old content into MessageHistory
+                MessageHistory.objects.create(
+                    message=old_message,
+                    old_content=old_message.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
