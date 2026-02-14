5b84d7c878e6e09c72afe98ecde9e2e54de4aa70
Moana <missn.hadebe@gmail.com>
2026-02-14 12:26:09 +0200

    Add RequestLoggingMiddleware and configure logging

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,3 +1,36 @@
+'chats.middleware.RequestLoggingMiddleware',
+
+
+
+
+
+
+import logging
+from datetime import datetime
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # Configure logger to write to requests.log
+        logging.basicConfig(
+            filename="requests.log",
+            level=logging.INFO,
+            format="%(message)s"
+        )
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logging.info(log_message)
+
+        response = self.get_response(request)
+        return response
+
+
+
+
+
+
 from datetime import datetime, timedelta
 from django.http import HttpResponseForbidden
 
--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -1,3 +1,21 @@
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
+]
+
+
+
+
+
+
 # Django REST Framework settings
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
